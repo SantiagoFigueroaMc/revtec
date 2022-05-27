12dca3591b2a5ccc8e16ec43b7c1bab8efbc27cf
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2022-05-26 23:55:32 -0400

    coutes corrected

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,4 @@ class Client < ApplicationRecord
   has_many :cars
 
   validates :name, presence: true
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,38 +12,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_161941) do
+ActiveRecord::Schema[7.0].define(version: 20_220_526_161_941) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "addresses", force: :cascade do |t|
-    t.string "street"
-    t.string "city"
-    t.string "state"
-    t.string "number"
-    t.string "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "client_id", null: false
-    t.index ["client_id"], name: "index_addresses_on_client_id"
+  create_table 'addresses', force: :cascade do |t|
+    t.string 'street'
+    t.string 'city'
+    t.string 'state'
+    t.string 'number'
+    t.string 'details'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'client_id', null: false
+    t.index ['client_id'], name: 'index_addresses_on_client_id'
   end
 
-  create_table "cars", force: :cascade do |t|
-    t.string "licence_plate"
-    t.date "last_revision_date"
-    t.bigint "client_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["client_id"], name: "index_cars_on_client_id"
+  create_table 'cars', force: :cascade do |t|
+    t.string 'licence_plate'
+    t.date 'last_revision_date'
+    t.bigint 'client_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['client_id'], name: 'index_cars_on_client_id'
   end
 
-  create_table "clients", force: :cascade do |t|
-    t.string "name"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'clients', force: :cascade do |t|
+    t.string 'name'
+    t.string 'phone'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "addresses", "clients"
-  add_foreign_key "cars", "clients"
+  add_foreign_key 'addresses', 'clients'
+  add_foreign_key 'cars', 'clients'
 end
